d7857e5324857e09cc1badf5d4f974af3eacfd46
vivinjayakar <vivinjayakar@gmail.com>
2026-04-06 20:10:42 +0530

    added .env file

--- demo/src/main/java/com/chatapp/config/WebSocketConfig.java
+++ demo/src/main/java/com/chatapp/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.chatapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -8,10 +9,13 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns(frontendUrl)
                 .withSockJS();
     }
 
